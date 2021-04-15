c8dd9c983f804389ab078e3ea1649d712d25859f
Logan Derrick  Intertek <logan.derrick@intertek.com>
2021-04-15 15:44:42 -0500

    Fixed MainComponent to include Header and Footer import

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 
 class Main extends Component {
   constructor(props) {
