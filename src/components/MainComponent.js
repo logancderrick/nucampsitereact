8213d26217e2c2f2f861af55e533c47508740c61
Logan Derrick  Intertek <logan.derrick@intertek.com>
2021-04-15 15:41:32 -0500

    Added Header and Footer React components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Navbar, NavbarBrand } from "reactstrap";
 import Directory from "./DirectoryComponent";
 import CampsiteInfo from "./CampsiteInfoComponent";
 import { CAMPSITES } from "../shared/campsites";
@@ -20,13 +19,10 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">NuCamp</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <Directory campsites={this.state.campsites} onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)} />
         <CampsiteInfo campsite={this.state.campsites.filter((campsite) => campsite.id === this.state.selectedCampsite)[0]} />
+        <Footer />
       </div>
     );
   }
